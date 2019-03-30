16b9c1890230d51fa91957aab237465662bf2ca2
yunjey0 <sehajyang@gmail.com>
2019-03-30 23:26:27 +0900

    fixed untracked files

--- vendor/cache/ruby/2.5.0/bin/safe_yaml.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-IF NOT "%~f0" == "~f0" GOTO :WinNT
-@"C:\Ruby25-x64\bin\ruby.exe" "C:/Users/muil/gitblog/vendor/cache/ruby/2.5.0/bin/safe_yaml" %1 %2 %3 %4 %5 %6 %7 %8 %9
-GOTO :EOF
-:WinNT
-@"C:\Ruby25-x64\bin\ruby.exe" "%~dpn0" %*
